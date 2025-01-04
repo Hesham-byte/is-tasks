44d7213681106bf93006b6b8aabf268f44e80da1
Hesham Fawzy <h3050571@gmail.com>
2025-01-04 20:50:40 +0200

    refactor index.html and main.js to dynamically import scripts on DOMContentLoaded

--- src/main.js
+++ src/main.js
@@ -2,22 +2,30 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/sweetalert2/dist/sweetalert2.min.css";
 import "../src/style.css";
 
-import "../node_modules/bootstrap/dist/js/bootstrap.min";
-import "../node_modules/sweetalert2/dist/sweetalert2.min";
+document.addEventListener("DOMContentLoaded", () => {
+  import("../node_modules/bootstrap/dist/js/bootstrap.min");
+  import("../node_modules/sweetalert2/dist/sweetalert2.min");
+  import("./Q1.js");
+  import("./Q2.js");
+  import("./Q4.js");
+  import("./Q5.js");
+  import("./Q6.js");
+  import("./Q7.js");
 
-document.querySelector("#app").innerHTML = `
-<div class="container">
-  <header id="header">
-    <h1 class="text-center">JavaScript Tasks</h1>
-  </header>
-  <div class="accordion" id="accordionExample">
-    <div id="Q1"></div>
-    <div id="Q2"></div>
-    <div id="Q3"></div>
-    <div id="Q4"></div>
-    <div id="Q5"></div>
-    <div id="Q6"></div>
-    <div id="Q7"></div>
-  </div>
-</div>
-`;
+  document.querySelector("#app").innerHTML = `
+    <div class="container">
+      <header id="header">
+        <h1 class="text-center">JavaScript Tasks</h1>
+      </header>
+      <div class="accordion" id="accordionExample">
+        <div id="Q1"></div>
+        <div id="Q2"></div>
+        <div id="Q3"></div>
+        <div id="Q4"></div>
+        <div id="Q5"></div>
+        <div id="Q6"></div>
+        <div id="Q7"></div>
+      </div>
+    </div>
+  `;
+});
